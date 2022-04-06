3aca907d7e983e2623c66b7b8b5278611e642cac
hereafter <tu.tong@outlook.com>
2022-04-06 18:28:18 +0800

    update test nuget packages introduce emptyobjectoobject converter

---
+++ src/WindowsApps.Toolkit.WinUI/UI/Converters/EmptyObjectToObjectConverter.cpp
@@ -0,0 +1,79 @@
+﻿#include "pch.h"
+#include "EmptyObjectToObjectConverter.h"
+#if __has_include("UI.Converters.EmptyObjectToObjectConverter.g.cpp")
+#include "UI.Converters.EmptyObjectToObjectConverter.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace winrt::Windows::Foundation;
+using namespace winrt::Windows::UI::Xaml::Interop;
+using namespace winrt::Microsoft::UI::Xaml;
+
+using namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::implementation;
+
+#pragma region Properties
+
+#define __WATWUIC winrt::WindowsApps::Toolkit::WinUI::UI::Converters
+
+DependencyProperty EmptyObjectToObjectConverter::__emptyValueProperty = DependencyProperty::Register(
+	L"EmptyValue", 
+	xaml_typename<IInspectable>(), 
+	xaml_typename<__WATWUIC::EmptyObjectToObjectConverter>(),
+	PropertyMetadata{nullptr}
+);
+
+DependencyProperty EmptyObjectToObjectConverter::__notEmptyValueProperty = DependencyProperty::Register(
+	L"NotEmptyValue", 
+	xaml_typename<IInspectable>(),
+	xaml_typename<__WATWUIC::EmptyObjectToObjectConverter>(),
+	PropertyMetadata{ nullptr }
+);
+
+DependencyProperty EmptyObjectToObjectConverter::EmptyValueProperty()
+{
+	return __emptyValueProperty;
+}
+
+DependencyProperty EmptyObjectToObjectConverter::NotEmptyValueProperty()
+{
+	return __notEmptyValueProperty;
+}
+
+#pragma endregion
+
+
+IInspectable EmptyObjectToObjectConverter::EmptyValue()
+{
+	return this->GetValue(__emptyValueProperty);
+}
+
+void EmptyObjectToObjectConverter::EmptyValue(IInspectable value)
+{
+	this->SetValue(__emptyValueProperty, value);
+}
+
+IInspectable EmptyObjectToObjectConverter::NotEmptyValue()
+{
+	return this->GetValue(__notEmptyValueProperty);
+}
+
+void EmptyObjectToObjectConverter::NotEmptyValue(IInspectable value)
+{
+	this->SetValue(__notEmptyValueProperty, value);
+}
+
+IInspectable EmptyObjectToObjectConverter::Convert(IInspectable const& value, TypeName const& targetType, IInspectable const& parameter, hstring const& language)
+{
+	return nullptr;
+}
+
+IInspectable EmptyObjectToObjectConverter::ConvertBack(IInspectable const& value, TypeName const& targetType, IInspectable const& parameter, hstring const& language)
+{
+	return nullptr;
+}
+
+
+bool EmptyObjectToObjectConverter::CheckValueIsEmpty(IInspectable value)
+{
+	return false;
+}
---
+++ src/WindowsApps.Toolkit.WinUI/UI/Converters/EmptyObjectToObjectConverter.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+
+#include "UI.Converters.EmptyObjectToObjectConverter.g.h"
+
+namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::implementation
+{
+    using namespace winrt;
+    using namespace winrt::Windows::Foundation;
+    using namespace winrt::Windows::UI::Xaml::Interop;
+    using namespace winrt::Microsoft::UI::Xaml;
+
+    struct EmptyObjectToObjectConverter : EmptyObjectToObjectConverterT<EmptyObjectToObjectConverter>
+    {
+        EmptyObjectToObjectConverter() = default;
+
+    public:
+        static DependencyProperty EmptyValueProperty();
+        static DependencyProperty NotEmptyValueProperty();    
+
+    public:
+        IInspectable EmptyValue();
+        void EmptyValue(IInspectable value);
+
+        IInspectable NotEmptyValue();
+        void NotEmptyValue(IInspectable value);
+
+    public: //IValueConverter
+        IInspectable Convert(IInspectable const& value, TypeName const& targetType, IInspectable const& parameter, hstring const& language);
+        IInspectable ConvertBack(IInspectable const& value, TypeName const& targetType, IInspectable const& parameter, hstring const& language);
+
+    public:
+        virtual bool CheckValueIsEmpty(IInspectable value);
+        
+
+    private:
+        static DependencyProperty __emptyValueProperty;
+        static DependencyProperty __notEmptyValueProperty;
+
+    };
+}
+
+namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::factory_implementation
+{
+    struct EmptyObjectToObjectConverter : EmptyObjectToObjectConverterT<EmptyObjectToObjectConverter, implementation::EmptyObjectToObjectConverter>
+    {
+    };
+}
