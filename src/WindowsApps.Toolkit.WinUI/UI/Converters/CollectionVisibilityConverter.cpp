5c9064a4a8d520a45f43fccb0cc898481fae04de
hereafter <tu.tong@outlook.com>
2022-04-07 21:17:55 +0800

    CollectionVisibilityConverter

---
+++ src/WindowsApps.Toolkit.WinUI/UI/Converters/CollectionVisibilityConverter.cpp
@@ -0,0 +1,14 @@
+﻿#include "pch.h"
+#include "CollectionVisibilityConverter.h"
+#if __has_include("UI.Converters.CollectionVisibilityConverter.g.cpp")
+#include "UI.Converters.CollectionVisibilityConverter.g.cpp"
+#endif
+
+using namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::implementation;
+
+
+CollectionVisibilityConverter::CollectionVisibilityConverter()
+{
+	this->EmptyValue(box_value(Visibility::Collapsed));
+	this->NotEmptyValue(box_value(Visibility::Visible));
+}
---
+++ src/WindowsApps.Toolkit.WinUI/UI/Converters/CollectionVisibilityConverter.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "UI.Converters.CollectionVisibilityConverter.g.h"
+#include "EmptyCollectionToObjectConverter.h"
+
+namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::implementation
+{
+    struct CollectionVisibilityConverter : CollectionVisibilityConverterT<CollectionVisibilityConverter, 
+                EmptyCollectionToObjectConverter>
+    {
+        CollectionVisibilityConverter();
+
+    };
+}
+
+namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::factory_implementation
+{
+    struct CollectionVisibilityConverter : CollectionVisibilityConverterT<CollectionVisibilityConverter, implementation::CollectionVisibilityConverter>
+    {
+    };
+}
