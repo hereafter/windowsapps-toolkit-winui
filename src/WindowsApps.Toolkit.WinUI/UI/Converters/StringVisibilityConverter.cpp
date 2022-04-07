a9b93b808e265a6929db02934146c3b5ba7f56a9
hereafter <tu.tong@outlook.com>
2022-04-07 21:32:27 +0800

    StringVisibilityConverter

---
+++ src/WindowsApps.Toolkit.WinUI/UI/Converters/StringVisibilityConverter.cpp
@@ -0,0 +1,13 @@
+﻿#include "pch.h"
+#include "StringVisibilityConverter.h"
+#if __has_include("UI.Converters.StringVisibilityConverter.g.cpp")
+#include "UI.Converters.StringVisibilityConverter.g.cpp"
+#endif
+
+using namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::implementation;
+
+StringVisibilityConverter::StringVisibilityConverter()
+{
+	this->EmptyValue(box_value(Visibility::Collapsed));
+	this->NotEmptyValue(box_value(Visibility::Visible));
+}
---
+++ src/WindowsApps.Toolkit.WinUI/UI/Converters/StringVisibilityConverter.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "UI.Converters.StringVisibilityConverter.g.h"
+#include "EmptyStringToObjectConverter.h"
+
+namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::implementation
+{
+    struct StringVisibilityConverter : StringVisibilityConverterT<StringVisibilityConverter, EmptyStringToObjectConverter>
+    {
+        StringVisibilityConverter();
+    };
+}
+
+namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::factory_implementation
+{
+    struct StringVisibilityConverter : StringVisibilityConverterT<StringVisibilityConverter, implementation::StringVisibilityConverter>
+    {
+    };
+}
