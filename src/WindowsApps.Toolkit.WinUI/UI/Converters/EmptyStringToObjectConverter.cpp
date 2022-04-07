0a3376532ae175d640a7a286c5db866707885545
hereafter <tu.tong@outlook.com>
2022-04-07 16:11:00 +0800

    EmptyObjectToObjectConverter

---
+++ src/WindowsApps.Toolkit.WinUI/UI/Converters/EmptyStringToObjectConverter.cpp
@@ -0,0 +1,13 @@
+﻿#include "pch.h"
+#include "EmptyStringToObjectConverter.h"
+#if __has_include("UI.Converters.EmptyStringToObjectConverter.g.cpp")
+#include "UI.Converters.EmptyStringToObjectConverter.g.cpp"
+#endif
+
+using namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::implementation;
+
+
+bool EmptyStringToObjectConverter::CheckValueIsEmpty(IInspectable value)
+{
+	return value && !unbox_value_or<hstring>(value, L"").empty();
+}
---
+++ src/WindowsApps.Toolkit.WinUI/UI/Converters/EmptyStringToObjectConverter.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "UI.Converters.EmptyStringToObjectConverter.g.h"
+#include "EmptyObjectToObjectConverter.h"
+
+namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::implementation
+{
+    using namespace winrt;
+    using namespace winrt::Windows::Foundation;
+    using namespace winrt::Windows::UI::Xaml::Interop;
+    using namespace winrt::Microsoft::UI::Xaml;
+
+    struct EmptyStringToObjectConverter : EmptyStringToObjectConverterT<EmptyStringToObjectConverter, EmptyObjectToObjectConverter>
+    {
+        EmptyStringToObjectConverter() = default;
+
+    public:
+        virtual bool CheckValueIsEmpty(IInspectable value);
+    };
+}
+
+namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::factory_implementation
+{
+    struct EmptyStringToObjectConverter : EmptyStringToObjectConverterT<EmptyStringToObjectConverter, implementation::EmptyStringToObjectConverter>
+    {
+    };
+}
