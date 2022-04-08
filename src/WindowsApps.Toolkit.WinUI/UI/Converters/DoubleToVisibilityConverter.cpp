7f97bbd7da7455ba76d24ba3e9bd0394bc747dcf
hereafter <tu.tong@outlook.com>
2022-04-08 12:30:16 +0800

    DoubleToVisibilityConverter

---
+++ src/WindowsApps.Toolkit.WinUI/UI/Converters/DoubleToVisibilityConverter.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include "DoubleToVisibilityConverter.h"
+#if __has_include("UI.Converters.DoubleToVisibilityConverter.g.cpp")
+#include "UI.Converters.DoubleToVisibilityConverter.g.cpp"
+#endif
+
+using namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::implementation;
+
+
+DoubleToVisibilityConverter::DoubleToVisibilityConverter()
+{
+	this->TrueValue(box_value(Visibility::Visible));
+	this->FalseValue(box_value(Visibility::Collapsed));
+	this->NullValue(box_value(Visibility::Collapsed));
+}
---
+++ src/WindowsApps.Toolkit.WinUI/UI/Converters/DoubleToVisibilityConverter.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "UI.Converters.DoubleToVisibilityConverter.g.h"
+#include "DoubleToObjectConverter.h"
+
+namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::implementation
+{
+    struct DoubleToVisibilityConverter : DoubleToVisibilityConverterT<DoubleToVisibilityConverter, DoubleToObjectConverter>
+    {
+        DoubleToVisibilityConverter();
+    };
+}
+
+namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::factory_implementation
+{
+    struct DoubleToVisibilityConverter : DoubleToVisibilityConverterT<DoubleToVisibilityConverter, implementation::DoubleToVisibilityConverter>
+    {
+    };
+}
