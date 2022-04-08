da6e57c76ea5b0800bf80bf47e767c05991239a2
hereafter <tu.tong@outlook.com>
2022-04-09 01:53:45 +0800

    StringFormatConverter wip

---
+++ src/WindowsApps.Toolkit.WinUI/UI/Converters/StringFormatConverter.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "StringFormatConverter.h"
+#if __has_include("UI.Converters.StringFormatConverter.g.cpp")
+#include "UI.Converters.StringFormatConverter.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace winrt::Windows::Foundation;
+using namespace winrt::Windows::UI::Xaml::Interop;
+using namespace winrt::Microsoft::UI::Xaml;
+using namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::implementation;
+
+
+IInspectable StringFormatConverter::Convert(IInspectable const& value, TypeName const& targetType, IInspectable const& parameter, hstring const& language)
+{
+	return nullptr;
+}
+
+IInspectable StringFormatConverter::ConvertBack(IInspectable const& value, TypeName const& targetType, IInspectable const& parameter, hstring const& language)
+{
+	return nullptr;
+}
---
+++ src/WindowsApps.Toolkit.WinUI/UI/Converters/StringFormatConverter.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "UI.Converters.StringFormatConverter.g.h"
+
+namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::implementation
+{
+    using namespace winrt;
+    using namespace winrt::Windows::Foundation;
+    using namespace winrt::Windows::UI::Xaml::Interop;
+    using namespace winrt::Microsoft::UI::Xaml;
+
+    struct StringFormatConverter : StringFormatConverterT<StringFormatConverter>
+    {
+        StringFormatConverter() = default;
+
+    public: //IValueConverter
+        IInspectable Convert(IInspectable const& value, TypeName const& targetType, IInspectable const& parameter, hstring const& language);
+        IInspectable ConvertBack(IInspectable const& value, TypeName const& targetType, IInspectable const& parameter, hstring const& language);
+    };
+}
+
+namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::factory_implementation
+{
+    struct StringFormatConverter : StringFormatConverterT<StringFormatConverter, implementation::StringFormatConverter>
+    {
+    };
+}
