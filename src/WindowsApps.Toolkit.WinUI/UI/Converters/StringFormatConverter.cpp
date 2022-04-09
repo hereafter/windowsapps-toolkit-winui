524744ca0e351c92e04dcd59b91f5a7b2cf17052
hereafter <tu.tong@outlook.com>
2022-04-10 01:04:27 +0800

    StringFormatConverter prototype

--- src/WindowsApps.Toolkit.WinUI/UI/Converters/StringFormatConverter.cpp
+++ src/WindowsApps.Toolkit.WinUI/UI/Converters/StringFormatConverter.cpp
@@ -4,7 +4,8 @@
 #include "UI.Converters.StringFormatConverter.g.cpp"
 #endif
 
-#include <format>
+#include <cstdarg>
+#include <wil/resource.h>
 
 using namespace winrt;
 using namespace winrt::Windows::Foundation;
@@ -12,13 +13,141 @@ using namespace winrt::Windows::UI::Xaml::Interop;
 using namespace winrt::Microsoft::UI::Xaml;
 using namespace winrt::WindowsApps::Toolkit::WinUI::UI::Converters::implementation;
 
+using namespace std;
 
-IInspectable StringFormatConverter::Convert(IInspectable const& value, TypeName const& targetType, IInspectable const& parameter, hstring const& language)
+IInspectable StringFormatConverter::Convert(IInspectable const& value, TypeName const&, IInspectable const& parameter, hstring const&)
 {	
-	return nullptr;
+	using namespace wil;
+
+	if (!value) return nullptr;
+	if (!parameter) return value;
+
+	auto format = unbox_value_or(parameter, L"");
+	auto&& f = format.c_str();
+
+	//TODO: std format not stable yet as C++ 20??
+
+	auto pv = value.try_as<IPropertyValue>();
+	if (!pv) return value;
+
+	auto type = pv.Type();	
+	switch (type)
+	{
+		case PropertyType::Boolean:
+		{
+			auto s = pv.GetBoolean() ? L"true" : L"false";
+			auto r = str_printf_failfast<unique_cotaskmem_string>(f, s);
+			return box_value(r.get());;
+		}
+		case PropertyType::Double:
+		{
+			auto v= pv.GetDouble();
+			auto r = str_printf_failfast<unique_cotaskmem_string>(f, v);
+			return box_value(r.get());;
+		}
+		case PropertyType::Single:
+		{
+			auto v = pv.GetSingle();
+			auto r = str_printf_failfast<unique_cotaskmem_string>(f, v);
+			return box_value(r.get());;
+		}
+
+		case PropertyType::UInt8:
+		{
+			auto v = pv.GetUInt8();
+			auto r = str_printf_failfast<unique_cotaskmem_string>(f, v);
+			return box_value(r.get());;
+		}
+
+		case PropertyType::Int16:
+		{
+			auto v = pv.GetInt16();
+			auto r = str_printf_failfast<unique_cotaskmem_string>(f, v);
+			return box_value(r.get());;
+		}
+
+		case PropertyType::UInt16:
+		{
+			auto v = pv.GetUInt16();
+			auto r = str_printf_failfast<unique_cotaskmem_string>(f, v);
+			return box_value(r.get());;
+		}
+
+		case PropertyType::Int32:
+		{
+			auto v = pv.GetInt32();
+			auto r = str_printf_failfast<unique_cotaskmem_string>(f, v);
+			return box_value(r.get());;
+		}
+
+		case PropertyType::UInt32:
+		{
+			auto v = pv.GetUInt32();
+			auto r = str_printf_failfast<unique_cotaskmem_string>(f, v);
+			return box_value(r.get());;
+		}
+
+		case PropertyType::Int64:
+		{
+			auto v = pv.GetInt64();
+			auto r = str_printf_failfast<unique_cotaskmem_string>(f, v);
+			return box_value(r.get());;
+		}
+
+		case PropertyType::UInt64:
+		{
+			auto v = pv.GetUInt64();
+			auto r = str_printf_failfast<unique_cotaskmem_string>(f, v);
+			return box_value(r.get());;
+		}
+
+		case PropertyType::Point:
+		{
+			auto point = pv.GetPoint();
+			wostringstream ss;
+			ss << point.X << "," << point.Y;
+			auto r = str_printf_failfast<unique_cotaskmem_string>(f, ss.str().c_str());
+			return box_value(r.get());;
+		}
+
+		case PropertyType::Size:
+		{
+			auto size = pv.GetSize();
+			wostringstream ss;
+			ss << size.Width << "," << size.Height;
+			auto r = str_printf_failfast<unique_cotaskmem_string>(f, ss.str().c_str());
+			return box_value(r.get());;
+		}
+		
+		case PropertyType::Rect:
+		{
+			auto rect = pv.GetRect();
+			wostringstream ss; 
+			ss << rect.X << "," << rect.Y << "," << rect.Width << "," << rect.Height;
+			auto r = str_printf_failfast<unique_cotaskmem_string>(f, ss.str().c_str());
+			return box_value(r.get());;
+		}
+
+		case PropertyType::String:
+		{
+			auto s = pv.GetString();
+			auto r = str_printf_failfast<unique_cotaskmem_string>(f, s.c_str());
+			return box_value(r.get());
+		}
+
+		default: //stringable
+		{
+			auto s=pv.try_as<IStringable>();
+			if (!s) return value;
+
+			auto r = str_printf_failfast<unique_cotaskmem_string>(f, s.ToString().c_str());
+			return box_value(r.get());
+		}
+	}
+
 }
 
-IInspectable StringFormatConverter::ConvertBack(IInspectable const& value, TypeName const& targetType, IInspectable const& parameter, hstring const& language)
+IInspectable StringFormatConverter::ConvertBack(IInspectable const&, TypeName const&, IInspectable const&, hstring const&)
 {
 	throw_hresult({ E_NOTIMPL });
 }
